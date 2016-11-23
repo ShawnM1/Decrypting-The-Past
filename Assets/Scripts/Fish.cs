89a9bf600d59d5cac8c58ca66fa5e695bed9f18c
James J Girard <dsfreak0109@gmail.com>
2016-11-22 21:21:36 -0500

    Added Movement to the Fish. -James Shawn

---
+++ Assets/Scripts/Fish.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class Fish : MonoBehaviour {
+
+    Vector2 originalPos;
+    public int Range = 10;
+    float leftBound = 0;
+    float rightBound = 0;
+    float direction = .1f;
+	// Use this for initialization
+	void Start () {
+        originalPos = this.transform.position;
+        ComputeBounds();
+	}
+    void ComputeBounds()
+    {
+        leftBound = this.transform.position.x - Range;
+        rightBound = this.transform.position.x + Range;
+    }
+	void FixedUpdate()
+    {
+        //this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + (Mathf.Sin(Time.deltaTime) / 180) * 10, this.transform.position.z);
+        if (direction > 0 && this.transform.position.x < rightBound)
+        {
+            Move(direction);
+        }
+
+        else if (direction < 0 && this.transform.position.x > leftBound)
+        {
+            Move(direction);
+        }
+        else
+        {
+            direction *= -1;
+            Move(direction);
+            this.transform.localScale = new Vector3(this.transform.localScale.x * -1, this.transform.localScale.y, this.transform.localScale.z);
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+        
+    }
+    void Move(float direction)
+    {
+        this.transform.position = new Vector3(this.transform.position.x + direction, this.transform.position.y, this.transform.position.z);
+    }
+    void OnDrawGizmos()
+    {
+        
+        Vector3 position = this.transform.position;
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(new Vector3(originalPos.x - Range, originalPos.y,-10), new Vector3(originalPos.x + Range, originalPos.y, -10));
+    }
+}
