fc2b5b194d2cc72082d1d4116ad244b3ddb2052a
Shawn McCall <mccalls2@go.stockton.edu>
2016-11-30 22:02:16 -0500

    Testing in game movie. See Caesar level for work in progress

---
+++ Assets/Scripts/CaesarCode/MoviePlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoviePlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void playMovie()
+    {
+        // this line of code will make the Movie Texture begin playing
+        ((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
+        GetComponent<AudioSource>().Play();
+
+    }
+}
