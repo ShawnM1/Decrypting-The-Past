ad3da22b15e6cc0471392831b467d087e7a86148
RobMonzo <monzor@go.stockton.edu>
2016-12-09 23:44:37 -0500

    Unit Testing Solution mapped and GTG - James

---
+++ DecryptingThePastUnitTests/CaesarCipher.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Text;
+using System;
+
+public class CaesarCipher {
+
+    private string alphabet = "abcdefghijklmnopqrstuvwxyz";
+    /// <summary>
+    /// Returns the alphabet string
+    /// </summary>
+    /// <returns></returns>
+    public string GetAlphabet()
+    {
+        return alphabet;
+    }
+    /// <summary>
+    /// This function can encrypt or decrypt a cipher depending wheather the key is positive.
+    /// See decrypt method.
+    /// </summary>
+    /// <param name="key"></param>
+    /// <param name="message"></param>
+    /// <returns></returns>
+    public string encryptCipher(int key, string message)
+    {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < message.Length; i++)
+        {
+            int localKey = key;
+            if(localKey < 0)
+            {
+                localKey = findLetterPositionInAlpha(message[i]) + (key + 26);
+            }
+            else
+            {
+                localKey = (findLetterPositionInAlpha(message[i]) + key);
+            }
+            builder.Append(alphabet[localKey % 26]);
+        }
+        return builder.ToString();
+    }
+    /// <summary>
+    /// Calls encryptCipher with negative key value.
+    /// </summary>
+    /// <param name="key">integer that is negated</param>
+    /// <param name="message">plaintext essage</param>
+    /// <returns>string ciphertext</returns>
+    public string decrypt(int key, string message)
+    {
+        return encryptCipher(key, message);
+    }
+    private int findLetterPositionInAlpha(char c)
+    {
+        for (int i = 0; i < alphabet.Length; i++)
+        {
+            if (c == alphabet[i])
+            {
+                System.Diagnostics.Debug.WriteLine(c + "i pos " + i);
+                return i;
+            }
+        }
+        return -1;
+    }
+}
